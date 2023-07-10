c8cddb21322861e92cadef7d7773c2703e1396ec
eunice_sin_h <71563392+EuniceHG@users.noreply.github.com>
2023-07-10 10:36:58 +0000

    FinishCode

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,10 @@
 import React from "react";
 import TrafficLight from "./TrafficLight";
-import Buttons from "./Buttons";
 
 //create your first component
 const Home = () => {
 	return (
-		<>
-			<TrafficLight />
-			<Buttons />
-		</>
+		<TrafficLight />
 	);
 };
 
